727deb2165b55c6ad7c24db9b7187e4a15d5ede6
francois lalay <francois.lalay@gmail.com>
2023-04-10 15:36:24 +0200

    add swagger-php and NelmioApiDocBundle

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,10 +9,9 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
+    
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->json(["test" => "bonjour je viens de tester une api"], status : 200);
     }
 }
