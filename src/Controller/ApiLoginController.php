5728ee3115302c81d2e19ff19a19cf748c9831ca
francois lalay <francois.lalay@gmail.com>
2023-05-12 15:58:33 +0200

    change config jwt

--- src/Controller/ApiLoginController.php
+++ src/Controller/ApiLoginController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ApiLoginController extends AbstractController
 {
-    #[Route('/api/login', name: 'app_api_login', methods:['POST'])]
+    #[Route('/api/login_check', name: 'app_api_login', methods:['POST'])]
     public function apiLogin(): Response
     {
         $user = $this->getUser();
