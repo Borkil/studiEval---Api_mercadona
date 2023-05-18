81085508a1a199a9b6d8a6cb0c4bdf85af5e17fe
francois lalay <francois.lalay@gmail.com>
2023-05-18 17:39:58 +0200

    add edit and delete route for user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Exception;
 use App\Entity\User;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -47,5 +48,62 @@ class UserController extends AbstractController
                  'message' => $e->getMessage()       
             ],Response::HTTP_BAD_REQUEST, ['Access-Control-Allow-Origin' => '*']);
         }
+    }
+
+    #[Route('/api/user/{id}', name: 'api_edit_user', methods: ['PUT'])]
+    public function edit(UserRepository $userRepository,int $id, Request $request, SerializerInterface $serializer, EntityManagerInterface $manager, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        try {
+            $user = $userRepository->find($id);
+
+            if(!$user){
+                return $this->json([
+                    'status' => Response::HTTP_NOT_FOUND,
+                    'message' => 'User not found'
+                ], Response::HTTP_NOT_FOUND);
+            }
+            $content = $serializer->deserialize($request->getContent(), User::class, 'json');
+
+            $plaintextPassword = $content->getPassword();
+            $hashedPassword = $passwordHasher->hashPassword($user, $plaintextPassword);
+            if($content->getRoles() === ['ROLE_USER']){
+               $roles = $content->getRoles();     
+            }else{
+                $roles = ['ROLE_USER', $content->getRoles()[0]];
+            }
+
+            $user->setEmail($content->getEmail())
+                ->setPassword($hashedPassword)
+                ->setRoles($roles);
+
+            $manager->flush();
+
+            return $this->json($user, Response::HTTP_ACCEPTED);
+
+        } catch (Exception $e) {
+            return $this->json(
+                ['status' => Response::HTTP_BAD_REQUEST,
+                 'message' => $e->getMessage()       
+            ],Response::HTTP_BAD_REQUEST);
+        }
+    }
+
+    #[Route('/api/user/{id}', name: 'api_delete_user', methods: ['DELETE'])]
+    public function remove(UserRepository $userRepository,int $id, EntityManagerInterface $manager): Response
+    {
+        $user = $userRepository->find($id);
+        if(!$user){
+            return $this->json([
+                'status' => Response::HTTP_NOT_FOUND,
+                'message' => 'User not found'
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        $manager->remove($user);
+        $manager->flush();
+        return $this->json([
+            "status" => Response::HTTP_ACCEPTED,
+            "message" => "success delete user"
+        ], Response::HTTP_ACCEPTED);
     }
 }
